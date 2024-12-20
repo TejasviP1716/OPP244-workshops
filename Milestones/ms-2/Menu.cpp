e9f2cffbb55338af2383f6d3974cebe612f21b7e
Tejasvi Parmar <prmrtej.2020@gmail.com>
2024-12-19 20:22:35 -0500

    Add files via upload

---
+++ Milestones/ms-2/Menu.cpp
@@ -0,0 +1,131 @@
+/**************************************************
+
+  Name- Tejasvi Nitinkumar Parmar
+  student id- 151236239
+  Email- tnparmar2@myseneca.ca
+  section- ZAA
+  Date- 21/11/2024
+
+***************************************************/
+
+
+
+
+
+#include <iostream>
+#include <cstring>
+#include "Menu.h"
+#include "Utils.h"
+#include "constants.h"
+
+using namespace std;
+
+namespace seneca {
+    MenuItem::MenuItem(const char* content, unsigned indent, unsigned indentSize, unsigned row) { //item implementatio
+        m_content = nullptr;
+        m_indent = indent;
+        m_indentSize = indentSize;
+        m_row = row;
+
+        if (content) {
+            m_content = new char[strlen(content) + 1];
+            strcpy(m_content, content);
+        }
+    }
+
+    MenuItem& MenuItem::operator=(const MenuItem&)
+    { 
+        return*this;
+    }
+
+    MenuItem::~MenuItem() {
+        delete[] m_content;
+    }
+
+    std::ostream& MenuItem::display(std::ostream& os) const {
+        if (m_content) {
+        
+            for (unsigned i = 0; i < m_indent * m_indentSize; i++) {
+                os << " ";
+            }
+
+       
+            if (m_row) {
+                if (m_row >= 10) {
+                    os << m_row << "- ";
+                }
+                else {
+                    os << " " << m_row << "- ";  //space for single digits number
+                }
+            }
+
+            os << m_content << endl;
+        }
+        return os;
+    }
+
+
+    Menu::Menu(const char* title, const char* exit, unsigned indent, unsigned indentSize)
+        : m_title(title, indent, indentSize, 0)
+        , m_exitOption(exit ? exit : "Exit", indent, indentSize, 0)
+        , m_selectionPrompt("> ", indent, indentSize, 0) {
+        m_indentNum = indent;
+        m_indentSize = indentSize;
+        m_numItems = 0;
+        for (unsigned i = 0; i < MaximumNumberOfMenuItems; i++) {
+            m_menuItems[i] = nullptr;
+        }
+    }
+
+    
+
+    Menu& Menu::operator=(const Menu&)
+    {
+        return*this;
+    }
+
+    Menu::~Menu() {
+        for (unsigned i = 0; i < m_numItems; i++) {
+            delete m_menuItems[i];
+            m_menuItems[i] = nullptr;
+        }
+    }
+
+    Menu& Menu::operator<<(const char* content) {
+        if (m_numItems < MaximumNumberOfMenuItems && content) {
+            m_menuItems[m_numItems] = new MenuItem(content, m_indentNum, m_indentSize, m_numItems + 1);
+            m_numItems++;
+        }
+        return *this;
+    }
+
+    size_t Menu::select() const {
+     
+        if (m_title.m_content) {
+            m_title.display(cout);
+        }
+
+        for (unsigned i = 0; i < m_numItems; i++) {
+            m_menuItems[i]->display(cout);
+        }
+
+        for (unsigned i = 0; i < m_indentNum * m_indentSize; i++) {
+            cout << " ";
+        }
+        cout << " 0- " << m_exitOption.m_content << endl;
+
+        for (unsigned i = 0; i < m_indentNum * m_indentSize; i++) {
+            cout << " ";
+        }
+        cout << "> ";
+
+        return static_cast<size_t>(getInt(0, m_numItems));
+    }
+
+    size_t operator<<(std::ostream& os, const Menu& menu) {
+        if (&os == &std::cout) {
+            return menu.select();
+        }
+        return 0;
+    }
+}
---
+++ Milestones/ms-2/Menu.h
@@ -0,0 +1,60 @@
+/**************************************************
+
+  Name- Tejasvi Nitinkumar Parmar
+  student id- 151236239
+  Email- tnparmar2@myseneca.ca
+  section- ZAA
+  Date- 21/11/2024
+
+***************************************************/
+
+
+#ifndef SENECA_MENU_H
+#define SENECA_MENU_H
+#include <iostream>
+#include "constants.h"
+
+namespace seneca {
+    class Menu; 
+
+    class MenuItem {
+        friend class Menu;
+        char* m_content;
+        unsigned m_indentSize;
+        unsigned m_indent;
+        unsigned m_row;
+
+        MenuItem(const char* content, unsigned indent, unsigned indentSize, unsigned row);
+        // Prevent copying by making private
+        MenuItem(const MenuItem&);
+        MenuItem& operator=(const MenuItem&);
+        ~MenuItem();
+        std::ostream& display(std::ostream& os) const;
+    };
+
+    class Menu {
+        unsigned m_indentNum;
+        unsigned m_indentSize;
+        unsigned m_numItems;
+        MenuItem* m_menuItems[MaximumNumberOfMenuItems];
+        MenuItem m_title;
+        MenuItem m_exitOption;
+        MenuItem m_selectionPrompt;
+
+    public:
+        Menu(const char* title, const char* exit = "Exit",
+            unsigned indent = 0, unsigned indentSize = 3);
+
+        // Prevent copying by making private
+        Menu(const Menu&);
+        Menu& operator=(const Menu&);
+        ~Menu();
+
+        Menu& operator<<(const char* content);
+        size_t select() const;
+    };
+
+ 
+    size_t operator<<(std::ostream& os, const Menu& menu);
+}
+#endif // !SENECA_MENU_H
