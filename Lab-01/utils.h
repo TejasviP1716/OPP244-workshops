21beb52a56a476b8fafe104bb16dd622c2f31794
Tejasvi Parmar <prmrtej.2020@gmail.com>
2024-12-19 20:05:51 -0500

    Add files via upload

---
+++ Lab-01/utils.h
@@ -0,0 +1,27 @@
+/***************************************
+Name - Tejasvi Parmar
+student id - 151236239
+email - tnparmar2@myseneca.ca
+section - ZAA
+
+******************************************/
+
+
+
+#ifndef NAMESPACE_UTILS_H // replace with relevant names
+#define NAMESPACE_UTILS_H
+#pragma once
+
+#ifndef SENECA_UTILS_H_
+#define SENECA_UTILS_H_
+
+
+namespace seneca {
+
+	void flushkeys();
+	bool yes();
+	void toLowerCase(char* des, const char* src);
+}
+
+#endif
+#endif
