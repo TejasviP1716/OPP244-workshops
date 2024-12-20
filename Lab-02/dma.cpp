baeac7607c85d8c0f832c5ef3f32b5092798aedc
Tejasvi Parmar <prmrtej.2020@gmail.com>
2024-12-19 20:10:04 -0500

    Add files via upload

---
+++ Lab-02/dma.cpp
@@ -0,0 +1,75 @@
+/***************************************
+Name - Tejasvi Parmar
+student id - 151236239
+email - tnparmar2@myseneca.ca
+section - ZAA
+
+******************************************/
+
+#include <iostream>
+#include "dma.h"
+
+using namespace std;
+namespace seneca
+{
+
+    void reverse() 
+    {
+        int size;
+        cout << "Enter the number of double values:\n";
+        cout << "> ";
+        cin >> size;
+
+        double* values = new double[size];   // array of doubles allocate dynamically.....
+
+        // Get the input values from here
+        for (int i = 0; i < size; i++) 
+        {
+            cout << (i + 1) << "> ";
+            cin >> values[i];
+        }
+      
+        for (int i = size - 1; i >= 0; i--)  //values will be printing in  reverse order
+        {
+            cout << values[i] << endl;
+        }
+
+        delete[] values;  // dynamically allocated array
+    }
+
+    Contact* getContact()
+    {
+
+        Contact* newContact = new Contact;
+
+        cout << "Name: ";
+        cin >> newContact->m_name;
+        cout << "Last name: ";
+        cin >> newContact->m_lastname;
+        cout << "Phone number: ";
+        cin >> newContact->m_phoneNumber;
+
+        return newContact;
+    }
+
+    void display(const Contact& C) 
+    {
+        cout << C.m_name << " " << C.m_lastname << ", +" << C.m_phoneNumber << endl;
+    }
+
+    void deallocate(Contact* C) 
+    {
+
+        delete C;
+    }
+
+    void setEmpty(Contact& C) 
+    {
+
+        C.m_name[0] = '\0';
+        C.m_lastname[0] = '\0';
+        C.m_phoneNumber = 0;
+    }
+
+}
+
---
+++ Lab-02/input.cpp
@@ -0,0 +1,70 @@
+/***************************************
+Name - Tejasvi Parmar
+student id - 151236239
+email - tnparmar2@myseneca.ca
+section - ZAA
+
+******************************************/
+
+
+#include <iostream>
+#include "input.h"
+
+using namespace std;
+namespace seneca
+{
+    int getInt()
+    {
+        int num = 0;
+        bool done = false;
+        do {
+            // extract an integer from the console and put it in num
+            cin >> num;
+            // if this action fails (the integer is unreadable)
+            if (cin.fail()) {
+                cout << "Bad integer entry, please try again:\n> ";
+                // clear the failure status to activate cin again
+                cin.clear();
+            }
+            else {
+                done = true;
+            }
+            // flush the keyboard buffer up to 1000 characters or '\n', whichever comes first
+            cin.ignore(1000, '\n');
+        } while (!done);
+        return num;
+    }
+
+    int getInt(int minimumVal, int maximumVal)
+    {
+        int num;
+        bool done = false;
+        do {
+            num = getInt();
+            if (num < minimumVal || num > maximumVal)
+            {
+                cout << "Invalid value, [" << minimumVal << "<ENTRY<" << maximumVal << "]\n> ";
+            }
+            else {
+                done = true;
+            }
+        } while (!done);
+        return num;
+    }
+    bool getInt(int& ref, int min, int max) 
+    {
+        bool done = false;
+        do {
+            ref = getInt();
+            if (ref < min || ref > max) 
+            {
+                cout << "Only enter values between " << min << " and " << max << ", inclusive:\n> ";
+            }
+            else {
+                done = true;
+            }
+        } while (!done);
+
+        return true;
+    }
+}
